4fe1a8f38d3deb6a37d365ec1427308cb003b0f4
Jihoo_Janett <janett1005@naver.com>
2024-05-14 18:55:14 +0900

    Refactor: detector과 model 명시

--- src/detection_representation.py
+++ src/detection_representation.py
@@ -4,7 +4,7 @@ from deepface import DeepFace
 import pickle
 
 
-def detection_representation_faces(year):
+def detection_representation_faces(year, detector, model):
     '''
     폴더의 모든 이미지에서 얼굴 detection후 encoding 값 dict로 저장
     파일명에 ASCII문자만 포함하는 이미지만 고려 
@@ -28,18 +28,20 @@ def detection_representation_faces(year):
     for image_idx, image_dir in enumerate(images_dir):
         # face detection -> alignment -> normalization -> face representation
         # return : list, 얼굴 하나당 dictionary 1개
-        try:
+        try: 
+            # if model in 'VGG'
+            # Default is base. Options: base, raw, Facenet, Facenet2018, VGGFace, VGGFace2, ArcFace
             face_representations = DeepFace.represent(image_dir,
-                                            detector_backend='retinaface',
-                                            normalization='base', model_name='VGG-Face')
+                                            detector_backend=detector,
+                                            normalization='base', model_name=model)
         except ValueError:
             continue
 
         for face_idx, face_representation in enumerate(face_representations):
-            crop_save_face(image_dir, image_idx, face_idx, face_representation['facial_area'], output_dir+f'/{year}')
+            crop_save_face(image_dir, image_idx, face_idx, face_representation['facial_area'], output_dir+f'/{year}_{detector}_{model}')
             face_representations_dict[f'image{image_idx}_face{face_idx}'] = face_representation['embedding']
     
-    with open(f'{output_dir}/{year}_face_representations.pickle','wb') as fw:
+    with open(f'{output_dir}/{year}_face_representations_{model}.pickle','wb') as fw:
         pickle.dump(face_representations_dict, fw)
 
 def crop_save_face(image_dir, image_idx, face_idx, facial_area, output_dir):
@@ -49,7 +51,7 @@ def crop_save_face(image_dir, image_idx, face_idx, facial_area, output_dir):
         os.makedirs(output_dir)
 
     image=Image.open(image_dir)
-    image = image.convert('RGB') # RGBA형식의 이미지는 RGB형식 이미지인 JPED파일로 저장이 불가능
+    image = image.convert('RGB') # RGBA형식의 이미지는 RGB형식 이미지인 JPEG파일로 저장이 불가능
 
     top, right, bottom, left = facial_area['y'], facial_area['x']+facial_area['w'], facial_area['y']+facial_area['h'], facial_area['x']
     face_image = image.crop((left, top, right, bottom))
@@ -57,6 +59,11 @@ def crop_save_face(image_dir, image_idx, face_idx, facial_area, output_dir):
     face_filename = f"image{image_idx}_face{face_idx}.jpg"
     face_image.save(os.path.join(output_dir, face_filename))
 
-
-for year in os.listdir('data/original_date_corrected'):
-        detection_representation_faces(year)
+# years = os.listdir('data/original_date_corrected')
+years=['2022', '2023']
+detectors = ['retinaface']#'opencv', 'retinaface','mtcnn', 'ssd','', 'yolov8', 'centerface', 'mediapipe'
+models = ['VGG-Face'] #'VGG-Face', 'Facenet', 'Facenet512', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'SFace' 'GhostFaceNet'
+for detector in detectors:
+    for model in models:
+        for year in years:
+            detection_representation_faces(year, detector, model)
