3cc269bc7ebb22eee67708d5f45ef70741cd637b
jihoo <janett1005@naver.com>
2024-05-22 19:10:24 +0900

    Feat: CACD face crop

--- src/detection_representation.py
+++ src/detection_representation.py
@@ -9,8 +9,8 @@ def detection_representation_faces(year, detector, model):
     폴더의 모든 이미지에서 얼굴 detection후 encoding 값 dict로 저장
     파일명에 ASCII문자만 포함하는 이미지만 고려 
     '''
-    input_dir = f'data/original_date_corrected/{year}'
-    output_dir = f'data/faces'
+    input_dir = f'data/CACD2000/Emma_Watson/{year}'
+    output_dir = f'data/CACD2000/Emma_Watson/{year}/crop'
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -33,16 +33,17 @@ def detection_representation_faces(year, detector, model):
             # Default is base. Options: base, raw, Facenet, Facenet2018, VGGFace, VGGFace2, ArcFace
             face_representations = DeepFace.represent(image_dir,
                                             detector_backend=detector,
+                                            expand_percentage = 30,
                                             normalization='base', model_name=model)
         except ValueError:
             continue
 
         for face_idx, face_representation in enumerate(face_representations):
-            crop_save_face(image_dir, image_idx, face_idx, face_representation['facial_area'], output_dir+f'/{year}_{detector}_{model}')
+            crop_save_face(image_dir, image_idx, face_idx, face_representation['facial_area'], output_dir)
             face_representations_dict[f'image{image_idx}_face{face_idx}'] = face_representation['embedding']
     
-    with open(f'{output_dir}/{year}_face_representations_{model}.pickle','wb') as fw:
-        pickle.dump(face_representations_dict, fw)
+    # with open(f'{output_dir}/{year}_face_representations_{model}.pickle','wb') as fw:
+    #     pickle.dump(face_representations_dict, fw)
 
 def crop_save_face(image_dir, image_idx, face_idx, facial_area, output_dir):
     """
@@ -50,7 +51,7 @@ def crop_save_face(image_dir, image_idx, face_idx, facial_area, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    image=Image.open(image_dir)
+    image = Image.open(image_dir)
     image = image.convert('RGB') # RGBA형식의 이미지는 RGB형식 이미지인 JPEG파일로 저장이 불가능
 
     top, right, bottom, left = facial_area['y'], facial_area['x']+facial_area['w'], facial_area['y']+facial_area['h'], facial_area['x']
@@ -60,8 +61,8 @@ def crop_save_face(image_dir, image_idx, face_idx, facial_area, output_dir):
     face_image.save(os.path.join(output_dir, face_filename))
 
 # years = os.listdir('data/original_date_corrected')
-years=['2022', '2023']
-detectors = ['retinaface']#'opencv', 'retinaface','mtcnn', 'ssd','', 'yolov8', 'centerface', 'mediapipe'
+years=['2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013']
+detectors = ['retinaface'] #'opencv', 'retinaface','mtcnn', 'ssd','', 'yolov8', 'centerface', 'mediapipe'
 models = ['VGG-Face'] #'VGG-Face', 'Facenet', 'Facenet512', 'OpenFace', 'DeepFace', 'DeepID', 'ArcFace', 'SFace' 'GhostFaceNet'
 for detector in detectors:
     for model in models:
